9cc10b06c0541d9ce70bc50edf4c82a6e901b339
GitHub Action <action@github.com>
2020-08-18 16:25:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/riih/gengroup/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/riih/gengroup/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.riih.gengroup.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.riih.gengroup.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/riih/gengroup/services/MyProjectService.kt
+++ src/main/kotlin/com/github/riih/gengroup/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.riih.gengroup.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.riih.gengroup.MyBundle
 
 class MyProjectService(project: Project) {
 
